82f2704053dc5479ee63e6115141d16632ec0810
platisam <platisam@gmail.com>
2023-09-04 16:15:57 +0200

    creating fetchTours function for fetching data using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-const url = "https://course-api.netlify.app/api/react-tours-project";
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
