9c7063071d87ee58d0004c006f175d4b00e333a5
platisam <platisam@gmail.com>
2023-09-06 13:53:33 +0200

    creating removeTour function for deleting tour

--- src/Tours.js
+++ src/Tours.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import Tour from "./Tour";
-const Tours = ({ tours }) => {
+const Tours = ({ tours, removeTour }) => {
   return (
     <section>
-      <div>
+      <div className="title">
         <h2>our tours</h2>
         <div className="underline"></div>
       </div>
       <div>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
         })}
       </div>
     </section>
